32666db9ae013c96eb13a653a301fffe6707a520
vitalii_iunev <vetale4ik@gmail.com>
2023-04-04 14:52:57 +0300

    add lambda - if(Delete < 18 age)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 
 public class Main {
@@ -11,9 +12,12 @@ public class Main {
         people.add(new Person("Коля", "Петров", 17));
         people.add(new Person("Маша", "Железнова", 40));
         people.add(new Person("Ирина", "Мамонтова",35));
+        people.add(new Person("Виталий", "Юнев", 14));
 
         System.out.println(people);
         Collections.sort(people, new PersonsNameLength());
+        System.out.println(people);
+        people.removeIf((Predicate<Person>)person -> {return person.getAge() < 18;});
         System.out.println(people);
     }
 }
